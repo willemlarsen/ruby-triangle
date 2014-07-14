e1bbc49fdf6f3c4ad07ba1b76a5776de35b67e1f
Willem Larsen <willemlarsen@gmail.com>
2014-07-14 14:39:02 -0700

    separate test functions into initializing and identifying

--- spec/triangles_spec.rb
+++ spec/triangles_spec.rb
@@ -2,11 +2,11 @@ require 'rspec'
 require 'triangles.rb'
 
 describe 'Triangle' do
-	it 'creates an instance of class Triangle' do
+	it 'creates an instance of class Triangle with three sides' do
 		test_triangle = Triangle.new(1, 1, 1)
 		expect(test_triangle).to be_an_instance_of Triangle
 	end
-	it 'initializes the triangle with the length of three sides' do
+	it 'identifies right triangles' do
 		test_triangle = Triangle.new(1, 2, 3)
 		expect(test_triangle.type).to eq("Right triangle")
 	end
