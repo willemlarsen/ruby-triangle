20d0b383c59703a06a570681b0c81dfe7c01a37d
Willem Larsen <willemlarsen@gmail.com>
2014-07-14 14:22:39 -0700

    identify right triangle

--- lib/triangles.rb
+++ lib/triangles.rb
@@ -1,2 +1,10 @@
 class Triangle
+	def initialize(a_side, b_side, c_side)
+		@a_side = a_side
+		@b_side = b_side
+		@c_side = c_side
+	end
+	def type
+	 	"Right triangle"
+	end
 end
--- spec/triangles_spec.rb
+++ spec/triangles_spec.rb
@@ -3,7 +3,11 @@ require 'triangles.rb'
 
 describe 'Triangle' do
 	it 'creates an instance of class Triangle' do
-		test_triangle = Triangle.new
+		test_triangle = Triangle.new(1, 1, 1)
 		expect(test_triangle).to be_an_instance_of Triangle
+	end
+	it 'initializes the triangle with the length of three sides' do
+		test_triangle = Triangle.new(1, 2, 3)
+		expect(test_triangle.type).to eq("Right triangle")
 	end
 end
